8332cf288b57d807ec377eb34e506d54a9d5da3a
kgc0120 <senru0508@gmail.com>
2021-05-13 00:13:45 +0900

    BracketRotation

---
+++ java_algorithm/src/main/java/monthly/code/challenge/BracketRotation.java
@@ -0,0 +1,37 @@
+package monthly.code.challenge;
+
+public class BracketRotation {
+
+    public static void main(String[] args) {
+        String param = "[](){}";
+        String param2 = "}]()[{";
+        String param3 = "[)(]";
+        String param4 = "}}}";
+        String param5 = "{}";
+        System.out.println(solution(param5));
+    }
+
+    public static int solution(String s) {
+        int answer = 0;
+        String temp = "";
+        String rotaion = "";
+        for (int i = 0; i < s.length() - 1; i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if(s.charAt(j) == ']' && !temp.contains("[")) {
+                    break;
+                } else if(s.charAt(j) == ')' && !temp.contains("(")) {
+                    break;
+                } else if(s.charAt(j) == '}' && !temp.contains("{")) {
+                    break;
+                }
+                temp += s.charAt(j);
+                if(temp.equals(s)) {
+                    answer++;
+                    temp = "";
+                }
+            }
+            s = s.substring(1, s.length()) + s.charAt(0);
+        }
+        return answer;
+    }
+}
