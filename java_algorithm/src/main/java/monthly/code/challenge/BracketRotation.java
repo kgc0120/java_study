04b779c78dd22076eb946bd0372fa4f750b502e3
kgc0120 <senru0508@gmail.com>
2021-05-24 21:52:46 +0900

    BracketRotation

--- java_algorithm/src/main/java/monthly/code/challenge/BracketRotation.java
+++ java_algorithm/src/main/java/monthly/code/challenge/BracketRotation.java
@@ -1,5 +1,12 @@
 package monthly.code.challenge;
 
+import java.util.Stack;
+
+/*
+* https://programmers.co.kr/learn/courses/30/lessons/76502
+* 괄호 회전하기
+*
+* */
 public class BracketRotation {
 
     public static void main(String[] args) {
@@ -8,7 +15,7 @@ public class BracketRotation {
         String param3 = "[)(]";
         String param4 = "}}}";
         String param5 = "{}";
-        System.out.println(solution2(param));
+        System.out.println(solution2(param2));
     }
 
     public static int solution(String s) {
@@ -36,27 +43,41 @@ public class BracketRotation {
     }
 
     public static int solution2(String s) {
-        StringBuffer sb = new StringBuffer();
         int answer = 0;
-        String temp = "";
-        String rotaion = "";
-        for (int i = 0; i < s.length() - 1; i++) {
-            for (int j = 0; j < s.length(); j++) {
-                if(s.charAt(j) == ']' && !temp.contains("[")) {
-                    break;
-                } else if(s.charAt(j) == ')' && !temp.contains("(")) {
-                    break;
-                } else if(s.charAt(j) == '}' && !temp.contains("{")) {
-                    break;
-                }
-                temp += s.charAt(j);
-                if(temp.equals(s)) {
-                    answer++;
-                    temp = "";
-                }
+        StringBuilder temp = new StringBuilder(s);
+        for(int i = 0; i < s.length(); i++) {
+            if(isCorrectStr(temp.toString())) {
+                answer += 1;
             }
-            s = s.substring(1, s.length()) + s.charAt(0);
+            temp.append(temp.charAt(0)).deleteCharAt(0);
         }
         return answer;
+    }
+
+    private static boolean isCorrectStr(String str) {
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+             char now = str.charAt(i);
+             if(now == '[' || now == '{' || now == '(') {
+                 stack.add(now);
+             } else if(!stack.isEmpty()){
+                if(now == ')' && stack.peek() == '(') {
+                    stack.pop();
+                }
+                else if(now == '}' && stack.peek() == '{') {
+                    stack.pop();
+                }
+                else if(now == ']' && stack.peek() == '[') {
+                    stack.pop();
+                }
+             } else {
+                 return false;
+             }
+        }
+        if(stack.isEmpty()) {
+            return true;
+        }
+        return false;
     }
 }
